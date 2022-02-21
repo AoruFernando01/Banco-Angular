b8c8812e80ba022e126f4d928ea1d8621c2d1f8a
Fernando <joaofernandoscsc@gmail.com>
2022-02-21 17:51:19 -0300

    Criado Form para transferencias,Extratos e Service

---
+++ src/app/extrato/extrato.component.ts
@@ -0,0 +1,20 @@
+import { Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TransferenciaService } from './services/transferencia.service';
+
+@Component({
+  selector: 'app-extrato',
+  templateUrl: './extrato.component.html',
+  styleUrls: ['./extrato.component.scss']
+})
+export class ExtratoComponent implements OnInit {
+
+  @Input() transferencias: any[];
+
+  constructor(private service: TransferenciaService) { }
+
+  ngOnInit() {
+    this.transferencias = this.service.transferencias;
+  }
+
+}
